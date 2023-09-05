47305c76628edd73b0c7ee8739e3f4ca1d961e20
kitts <kittsmakokha@gmail.com>
2023-09-05 12:03:14 +0000

    server file, routes and ctrls dir

---
+++ controllers/AppController.js
@@ -0,0 +1,39 @@
+const { redisClient } = require('../utils/redis');
+const { db } = require('../utils/db');
+
+class AppController {
+  static async getStatus(req, res) {
+    try {
+      const isRedisAlive = await redisClient.isAlive(); 
+      const isDbAlive = await db.isAlive(); 
+      if (isRedisAlive && isDbAlive) {
+        return res.status(200).json({ redis: true, db: true }); // Both Redis and DB are alive
+      } else if (isRedisAlive && !isDbAlive) {
+        return res.status(200).json({ redis: true, db: false }); // Redis is alive, but DB is not
+      } else if (!isRedisAlive && isDbAlive) {
+        return res.status(200).json({ redis: false, db: true }); // Redis is not alive, but DB is
+      } else {
+        return res.status(500).json({ redis: false, db: false }); // Both Redis and DB are not alive
+      }
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ redis: false, db: false }); // An error occurred
+    }
+  }
+
+
+  static async getStats(req, res) {
+    try {
+      // Fetch the number of users and files from your database collections
+      const numberOfUsers = await db.collection('users').countDocuments();
+      const numberOfFiles = await db.collection('files').countDocuments();
+
+      return res.status(200).json({ users: numberOfUsers, files: numberOfFiles });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ users: 0, files: 0 });
+    }
+  }
+}
+
+module.exports = AppController;
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware for parsing JSON requests
+app.use(express.json());
+
+// Load routes from the routes/index.js file
+const routes = require('./routes');
+
+// Use the routes
+app.use('/', routes);
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
